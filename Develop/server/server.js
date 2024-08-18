10f35527b241cd83dcbf12830e3a868a87e6023b
ndzamboni <ndzamboni@gmail.com>
2024-08-18 13:07:19 -0400

    ran build and fixed some routing issues too

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Correct the path to serve the client build files
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
